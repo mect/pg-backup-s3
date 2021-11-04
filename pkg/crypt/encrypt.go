9d6fbd1e710994ea9e2d2bd91c7541123c4abb67
Maartje Eyskens <maartje@eyskens.me>
2021-11-04 10:46:22 +0100

    Add better errors in decryptor

--- pkg/crypt/encrypt.go
+++ pkg/crypt/encrypt.go
@@ -6,6 +6,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -52,7 +53,7 @@ func EncryptFile(path, outPath, pass string) error {
 func DecryptFile(path, outPath, pass string) error {
 	ciphertext, err := ioutil.ReadFile(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read file: %w", err)
 	}
 
 	h := sha256.New()
@@ -60,28 +61,28 @@ func DecryptFile(path, outPath, pass string) error {
 
 	block, err := aes.NewCipher(key[:32])
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create cipher: %w", err)
 	}
 
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create gcm: %w", err)
 	}
 	nonce, ciphertext := ciphertext[:gcm.NonceSize()], ciphertext[gcm.NonceSize():]
 
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to decrypt: %w", err)
 	}
 
 	// create a new file for saving the encrypted data.
 	f, err := os.Create(outPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create file: %w", err)
 	}
 	_, err = io.Copy(f, bytes.NewReader(plaintext))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to write to file: %w", err)
 	}
 
 	return nil
